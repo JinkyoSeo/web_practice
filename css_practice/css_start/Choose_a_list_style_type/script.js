a7db869ba04d0b501ab8aaab365f004946ca23bf
DESKTOP-LI8JNPR\seo <shhsjk2002@gmail.com>
2023-05-29 18:22:44 +0900

    css block task1 - html color change using javascript

--- css_practice/css_start/Choose_a_list_style_type/script.js
+++ css_practice/css_start/Choose_a_list_style_type/script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container");
+const para = documnet.querySelector("p");
+
 container.addEventListener("change", (event) => {
   const list = document.querySelector("ol");
   list.setAttribute("style", `list-style-type: ${event.target.value}`);
