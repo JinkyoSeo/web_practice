b6ca32e168a5cffcae9283d8688a4ab7866b6373
DESKTOP-LI8JNPR\seo <shhsjk2002@gmail.com>
2023-06-09 14:56:38 +0900

    js- bounce ball 예제 업데이트 -> ball 없애는 EvilCirle 추가, 남아 있는 ball 개수 출력

--- css_practice/css_start/js_bounce_ball/main.js
+++ css_practice/css_start/js_bounce_ball/main.js
@@ -19,6 +19,7 @@ function randomRGB() {
 }
 
 // 최상위 클래스로 Shape 생성
+// 좌표, 속도를 가짐
 class Shape{
    constructor(x, y, velX, velY){
       this.x = x;
@@ -28,24 +29,29 @@ class Shape{
    }
 }
 
-// Shape에서 상속받음
-// boolean exists 추가
+// Ball
+// Shape 모든 속성 상속
+// color, boolean exists 속성 추가
 class Ball extends Shape{
 
-   constructor(x, y, velX, velY, color, size) {
+   constructor(x, y, velX, velY, color, size)
+    {
       super(x, y, velX, velY);
       this.color = color;
       this.size = size;
       this.exists = true;
    }
 
+   // 원 그리기
    draw() {
-      ctx.beginPath();
-      ctx.fillStyle = this.color;
-      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-      ctx.fill();
-   }
+      ctx.beginPath();            // 새로운 경로를 만든다. 도형을 이루는(선, 아치)의 집합을 경로로 가짐
+                                  // 이 메소드가 호출 될 때 마다 하위 경로 모음은 초기화 -> 새로운 도형을 그릴 수 있다.
+      ctx.fillStyle = this.color; // 색
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);  // 중점 좌표(x,y), 반지름, startAngle, endAngle
+      ctx.fill();                 // 색 채우기
+   }  
 
+   // 원이 벽에 부딪힌 경우
    update() {
       if ((this.x + this.size) >= width) {
          this.velX = -(Math.abs(this.velX));
@@ -67,6 +73,7 @@ class Ball extends Shape{
       this.y += this.velY;
    }
 
+   // 공끼리 부딪힌 경우
    collisionDetect() {
       for (const ball of balls) {
          if (!(this === ball) && ball.exists) {
@@ -83,12 +90,16 @@ class Ball extends Shape{
 
 }
 
+// 볼 없애는 원
 class EvilCircle extends Shape{
-   constructor(){
+   constructor(x, y){
       super(x, y, 20, 20);
+      this.x = x;
+      this.y = y;
       this.color = 'white';
       this.size = 10;
 
+      // wasd를 누르면 원이 이동 -> class 안에다 넣네
       window.addEventListener("keydown", (e) => {
          switch (e.key) {
            case "a":
@@ -107,6 +118,7 @@ class EvilCircle extends Shape{
        });
    }
 
+   // 원 그리기 -> stroke사용 -> 원 안이 빈 원?
    draw() {
       ctx.beginPath();
       ctx.lineWidth = 3;
@@ -115,46 +127,58 @@ class EvilCircle extends Shape{
       ctx.stroke();
    }
 
+   // 사방 벽에 부딪힌 경우
    checkBounds() {
+      // 오른쪽 벽에 부딪힘 -> 잘 작동
       if ((this.x + this.size) >= width) {
          this.x = width - this.size;
       }
-
+      // 왼쪽 벽에 부딪힘 -> 잘 작동
       if ((this.x - this.size) <= 0) {
-         this.x = size;
+         this.x = this.size;
       }
-
+      // 아래쪽 벽에 부딪힘 -> 잘 작동
       if ((this.y + this.size) >= height) {
-         this.y = this.size;
+         this.y = height - this.size;
       }
-
+      // 위쪽 벽에 부딪힘 -> 잘 작동
       if ((this.y - this.size) <= 0) {
-         this.y = this.height - this.size;
+         this.y = this.size;
       }
    }
 
+   // 원이 볼을 부딪힌 경우 -> 잡아 먹음
    collisionDetect() {
       for (let i = 0; i < balls.length; i++) {
-         if (!balls[i].exists) {
-            // 존재 안하면 -> 이미 먹힘 지움
-            balls.splice(i, 1);
-            if (distance < this.size + ball.size) {
+         if (balls[i].exists) {
+            const dx = this.x - balls[i].x;
+            const dy = this.y - balls[i].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < this.size + balls[i].size) {
               balls[i].exists = false;
+              balls.splice(i, 1);
             }
          }
       }
+   
    }
 }
 
-
+// 볼 리스트 생성
 const balls = [];
-const evilCircle = new EvilCircle();
+// Evilcircle 생성
+const evilCircle = new EvilCircle(   
+   random(10 ,width - 10),
+   random(10 ,height - 10),
+);
 
+// 볼이 25개 생성된 때 까지 경우 반복
+// 볼 생성
 while (balls.length < 25) {
    const size = random(10,20);
    const ball = new Ball(
       // ball position always drawn at least one ball width
-      // away from the edge of the canvas, to avoid drawing errors
+      // away from the edge of the canvas, to avoid drawing errors,
       random(0 + size,width - size),
       random(0 + size,height - size),
       random(-7,7),
@@ -166,25 +190,37 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+// 공 개수 남은거 출력
+const para = document.querySelector("p");
+
+// 게임 반복되는 동안 루프
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
    ctx.fillRect(0, 0,  width, height);
 
+   // 공 움직임을 계속 그리기
    for (const ball of balls) {
      ball.draw();
      ball.update();
      ball.collisionDetect();
 
+     evilCircle.draw();
+     evilCircle.checkBounds();
+     evilCircle.collisionDetect();
+     
+
+     para.textContent = `ball count: ${balls.length}`;
+     para.append();
+     
+
      
    }
-
-   evilCircle.draw();
-   evilCircle.checkBounds();
-   evilCircle.collisionDetect();
-
-   
-
+   // 브라우저에게 수행하기를 원하는 애니메이션을 알리고
+   // 다음 리페인트 바로 전에 브라우저가 애니메이션을 업데이트할 지정된 함수를
+   // 호출하도록 요청
+   // requestAnimationFrame(반복할 함수)
    requestAnimationFrame(loop);
 }
 
+// 게임 시작
 loop();
