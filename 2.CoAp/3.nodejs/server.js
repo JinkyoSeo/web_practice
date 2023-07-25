f107c71776a0522460d6b6beb34eb30cf016cdae
DESKTOP-LI8JNPR\seo <shhsjk2002@gmail.com>
2023-07-25 14:41:06 +0900

    nodejs&mongo: 해당 리스트 삭제 버튼 누를 때, 리스트의 id와 db id를 연동시켜서 삭제 동작 작동 시키기

--- 2.CoAp/3.nodejs/server.js
+++ 2.CoAp/3.nodejs/server.js
@@ -12,9 +12,9 @@ MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.75bsb1m.mongodb.net/?
     db = client.db('todoapp');
 
     // db에 자료 추가 할때
-    db.collection('post').insertOne({이름 : 'John', _id : 100}, function(에러, 결과){
-        console.log('저장완료');
-    });
+    // db.collection('post').insertOne({/*이름 : 'John', _id : 100*/}, function(에러, 결과){
+    //     console.log('저장완료');
+    // });
 
     //서버띄우는 코드 여기로 옮기기
     app.listen('8080', function(){
@@ -34,7 +34,7 @@ app.get('/write', function(요청, 응답){
 
 app.get('/list', function(요청, 응답){
     db.collection('post').find().toArray(function(에러, 결과){
-        console.log(결과)
+        //console.log(결과)
         응답.render('list.ejs', {posts : 결과})
     })
 })
@@ -54,4 +54,11 @@ app.post('/add', function(요청, 응답){
     });
 });
 
-
+app.delete('/delete', function(요청, 응답){
+    요청.body._id = parseInt(요청.body._id);
+    db.collection('post').deleteOne(요청.body, function(에러, 결과){
+        console.log('삭제완료');
+    });
+    
+    응답.send('삭제완료');
+})
