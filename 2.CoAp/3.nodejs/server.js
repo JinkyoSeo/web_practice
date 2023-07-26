90a6e1b963f6c068f9f4b6c75882b0de99f8a39b
DESKTOP-LI8JNPR\seo <shhsjk2002@gmail.com>
2023-07-26 14:37:44 +0900

    nodejs&mongo: passport 라이브러리를 사용해서 로그인 기능 구현하기 -> 로그인 페이지 만들기

--- 2.CoAp/3.nodejs/server.js
+++ 2.CoAp/3.nodejs/server.js
@@ -7,12 +7,23 @@ app.use(express.urlencoded({extended: true}));
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
 
-// 노드에게 publick파일이 있는것을 알려줌
+// 노드에게 public파일이 있는것을 알려줌
 app.use('/public', express.static('public'));
 
 // method-override 사용하기 위해서 -> PUT(수정) method를 사용하기 위해서
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
+
+// 로그인을 위한
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+// app.use -> 미들웨어를 쓰겠다라는 뜻 -> 미들 웨어란? 요청과 응답사이에 뭔가 실행시키는 코드
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+// -- 로그인
 
 var db;
 MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.75bsb1m.mongodb.net/?retryWrites=true&w=majority',{ useUnifiedTopology: true }, function(에러, client){
@@ -60,6 +71,10 @@ app.get('/edit/:id', function(요청, 응답){
     });
   });
 
+app.get('/login', function(요청, 응답){
+    응답.render('login.ejs');
+});
+
 app.post('/add', function(요청, 응답){
     db.collection('counter').findOne({name: '게시물갯수'}, function(에러, 결과){
         var 총게시물갯수 = 결과.totalPost;
@@ -71,6 +86,14 @@ app.post('/add', function(요청, 응답){
             });
         });
     });
+});
+
+app.post('/login',passport.authenticate('local', {failureRedirect : '/fail'}) , function(요청, 응답){
+    // passport 어쩌구~ : passport 라이브러리가 제공하는 아이디비번인증 코드 
+    //                   -> 응답해주기 전에 local 방식으로 아이디 비번을 인증해주세요 
+    //                   -> failureRedirect : 실패시 이동시켜줄 경로
+    
+    응답.redirect('/'); // redirect() 다른 페이지로 이동 시켜줌
 });
 
 app.delete('/delete', function(요청, 응답){
@@ -85,9 +108,30 @@ app.delete('/delete', function(요청, 응답){
 app.put('/edit', function(요청, 응답){
     db.collection('post').updateOne({_id : parseInt(요청.body.id)}, {$set : {제목 : 요청.body.title, 날짜 : 요청.body.date}}, function(에러, 결과){
         console.log('수정 완료');
-        console.log(요청.body);
+        //console.log(요청.body);
         응답.redirect('/list');
     });
 });
+
+//                로컬 방식으로 아이디/ 비번 검사
+passport.use(new LocalStrategy({
+    // 설정부분
+    usernameField: 'id',      // 사용자가 제출한 아이디가 어디 적혔는지
+    passwordField: 'pw',      //  ..            비번이      ..
+    session: true,            // 세션을 만들건지
+    passReqToCallback: false, // 아이디/비번 말고 다른 정보 검사가 필요한지
+  }, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+      if (에러) return done(에러)
+  
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (입력한비번 == 결과.pw) {
+        return done(null, 결과)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }
+    })
+  }));
 
 
